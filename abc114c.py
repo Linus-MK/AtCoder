f1cde1c1f72566a86a6153f1c5da77c977862760
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2019-02-09 16:07:09 +0900

    ABC114 C問題 リファクタリング

--- abc114c.py
+++ abc114c.py
@@ -8,20 +8,15 @@ import itertools
 from functools import reduce
 
 n = int(input())
-ketasuu = len(str(n))
 ans = 0
 
-base = itertools.product('357', repeat=ketasuu)
-# baseはタプルからなるリスト
-
-for i in base:
-	if len(set(i))== 3: # 3種類の数字が1回以上使われている
-		i_int = int(reduce(lambda x, y: x + y, i))
-		if i_int <= n:
-			ans += 1
-
-for k in range(3, ketasuu):
-	temp = 3**k - 3*1**k - 3 * (2**k - 2* 1**k)
-	ans += temp
+for length in range(3, len(str(n)) +1):
+	base = itertools.product('357', repeat=length)
+	# baseはタプルからなるリスト
+	for i in base:
+		if len(set(i))== 3: # 3種類の数字が1回以上使われている
+			i_int = int(reduce(lambda x, y: x + y, i))
+			if i_int <= n:
+				ans += 1
 
 print(ans)
