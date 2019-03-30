9c92b52510a034823ca5c67518aa5cb23eddc95a
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2019-03-30 15:25:10 +0900

    ABC122 C問題 累積和の解法を更新

--- abc122c_cumulative_sum.py
+++ abc122c_cumulative_sum.py
@@ -4,23 +4,22 @@
 # 競技中に一瞬思いついたが、10万の配列を作ったときにメモリが足りるかを懸念して避けてしまった
 # 10万くらいなら余裕で足りるよ
 
-# printしてみた配列の大きさ：
-# 提出時の表示： 実行時間とメモリ 878 ms	6104 KB https://atcoder.jp/contests/abc122/submissions/4758777
+# printしてみた配列の大きさ：約800000バイト
+# 提出時の表示： 実行時間とメモリ 853 ms	6084 KB https://atcoder.jp/contests/abc122/submissions/4758805
 
 N,Q =  map(int, input().split())
 S = input()
 
-cumsum = []
+cumsum = [0] * N
 # cumsum[i] := (0-indexで)0文字目からi文字目までに含まれる部分列"AC"の個数
 # (0-indexで)l文字目からr文字目までに含まれる部分列"AC"の個数は、cumsum[r] -cumsum[l]である
 # l-1文字目とl文字目が"AC"になるケースも除外したいので、cumsum[l]を引くこと
 
-cumsum.append(0)
 summ = 0
 for i in range(1,N):
 	if S[i-1] == "A" and S[i] == "C":
 		summ +=1
-	cumsum.append(summ)
+	cumsum[i] = summ
 
 for _ in range(Q):
 	left, right = map(int, input().split())
@@ -28,3 +27,7 @@ for _ in range(Q):
 	right -= 1
 	print(cumsum[right] - cumsum[left])
 
+# import sys
+# print(sys.getsizeof(cumsum))
+# 10000文字を入力した場合の表示結果が80064。 (8×要素数+64)バイトであろう。
+# 制約上限の100000文字で800000バイトなので、10^9バイトの制限には余裕で収まる。
