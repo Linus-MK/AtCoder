9d278e2b3a970f07c7dca35d0c96c870dd1b2116
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2019-04-22 23:01:41 +0900

    ABC109 C問題 python3.4に対応

--- abc109c.py
+++ abc109c.py
@@ -1,11 +1,13 @@
-import math
-
+# import math
+import fractions
 n, start = list(map(int, input().split()))
 x = list(map(int, input().split()))
 
 ans = abs(start - x[0])
 for i in range(1, n):
 	dist = abs(start - x[i])
-	ans = math.gcd(ans, dist)
+	# ans = math.gcd(ans, dist)
 	# Python 3.5以上では正解になるはずだが、AtCoderでは3.4.3なのでmath.gcdが使えず、ランタイムエラーになる
+	# 3.4以前ではfractions.gcdを使う必要がある。fractions.gcdは3.5以降では非推奨である
+	ans = fractions.gcd(ans, dist)
 print(ans)
