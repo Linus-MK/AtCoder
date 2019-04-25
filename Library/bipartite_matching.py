1f80c2fefe3a9ed57e2bc720c63b4924f51079f7
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2019-04-25 22:47:34 +0900

    ライブラリ：二部グラフの最大マッチング

--- Library/bipartite_matching.py
+++ Library/bipartite_matching.py
@@ -1,13 +1,30 @@
 # 二部グラフに対する最大マッチング
 # 二部グラフの最大独立集合（=最大安定集合）、最小点カバーの問題は最大マッチング問題に帰着できる(蟻本p.198)
 
-# 蟻本p.196をpythonに移植
+# 蟻本p.197をpythonに移植
 
 V = -1 #頂点数
-MAX_V = 10000 #最大頂点数
+MAX_V = 10000 #最大頂点数。ここでは仮
 G[MAX_V] = [] #グラフの隣接リスト表現
 match[MAX_V] = [] #マッチングのペア
 used[MAX_V] = [] #DFSで既に調べたかのフラグ
+
+# uとvを結ぶ辺をグラフに追加する
+def add_edge(u, v):
+	G[u].append(v)
+	G[v].append(u)
+
+# 増加パスをDFS（深さ優先探索）で探す
+def dfs(v):
+	used[v] = True
+	for u in G[v]:
+		w = match[u]
+		if(w<0 or (!used[w] and dfs(w) ) ):
+			match[v] = u
+			match[u] = v
+			return True
+
+	return False
 
 
 # 二部グラフの最大マッチングを求める
