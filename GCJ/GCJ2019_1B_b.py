a0c5f5aa42eaa23d1eb5d266d246db7bd0cc22b1
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2019-05-04 00:42:42 +0900

    Google Code Jam 2019 Round1B B問題(visible+hidden まだWA)

--- GCJ/GCJ2019_1B_b.py
+++ GCJ/GCJ2019_1B_b.py
@@ -1,6 +1,33 @@
-# visible
-
+# visible+hidden
+# https://codingcompetitions.withgoogle.com/codejam/round/0000000000051706/0000000000122837#
 t, w = map(int, input().split())
+
+bit_mask = 0b1111111 #127, binary
+
+for _ in range(t):
+	print(210)
+	ans210 = int(input())
+	print(56)
+	ans56 = int(input())
+
+	r4 = ans210 >> 52
+	r5 = (ans210 >> 42) & bit_mask
+	r6 = (ans210 >> 35) & bit_mask
+
+	r1 = ans56 >> 56
+	r2 = (ans56 >> 28) & bit_mask
+#	r3 = ((ans56 - (r4<<14 + r5<<11 + r6<<9))>>18) & bit_mask
+	r3 = ((ans56 )>>18) & bit_mask
+# 引いた効果が出ていない（=直上の2行が同じことをしている）ように見える。なんでや……
+
+	print("{0} {1} {2} {3} {4} {5}".format(r1,r2,r3,r4,r5,r6) )
+
+	result = int(input())
+	if result != 1:
+		exit()
+
+
+'''
 if w != 6:
 	exit()
 
@@ -33,3 +60,4 @@ for _ in range(t):
 	result = int(input())
 	if result != 1:
 		exit()
+'''
