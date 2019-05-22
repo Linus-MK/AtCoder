dd5a117c7f25fcfd2815a9c42b23f8fd4b0c3938
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2019-05-23 00:08:36 +0900

    ABC062 C問題(依然としてWA)

--- arc062c.py
+++ arc062c.py
@@ -15,7 +15,11 @@ ans = [vote_ratio[0][0], vote_ratio[0][1]]
 
 for i in range(1,n):
 	bairitsu = max(ans[0] / vote_ratio[i][0], ans[1] / vote_ratio[i][1])
-	bairitsu = math.ceil(bairitsu)
+	# bairitsu = math.ceil(bairitsu)
+	bairitsu =  int(bairitsu)
+	if vote_ratio[i][0] * bairitsu < ans[0] \
+	or vote_ratio[i][1] * bairitsu < ans[1]:
+		bairitsu += 1
 	ans = [vote_ratio[i][0] * bairitsu, vote_ratio[i][1] * bairitsu]
 
 print(sum(ans))
