99242d00e5741b1ce9cc6430955663a47c6bbcd6
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2019-05-23 00:02:07 +0900

    ABC062 C問題(WA)

---
+++ arc062c.py
@@ -0,0 +1,21 @@
+# WA math.ceilは浮動小数点演算の影響を受けるので、正しく切り上げできないときがある
+# >>> math.ceil((12345678901234*456+0.1)/12345678901234)
+# 456
+# >>> math.ceil((12345678901234*456+1)/12345678901234)
+# 457
+
+import math
+
+n = int(input())
+vote_ratio = [ list(map(int, input().split())) for _ in range(n)]
+# print(vote_ratio)
+
+# ans = vote_ratio[0] は同じオブジェクトを参照してしまうので避ける
+ans = [vote_ratio[0][0], vote_ratio[0][1]]
+
+for i in range(1,n):
+	bairitsu = max(ans[0] / vote_ratio[i][0], ans[1] / vote_ratio[i][1])
+	bairitsu = math.ceil(bairitsu)
+	ans = [vote_ratio[i][0] * bairitsu, vote_ratio[i][1] * bairitsu]
+
+print(sum(ans))
