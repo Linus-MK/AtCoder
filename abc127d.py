63e5781600f791a3b6c1d3d81719f951eca8d535
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2019-06-01 18:11:36 +0900

    ABC127 D問題(TLE-2)

--- abc127d.py
+++ abc127d.py
@@ -1,17 +1,18 @@
 # ひとまず毎回ソートするバージョン
 # Python 3, pypy ともにTLE
+import bisect
 
 n, m = list(map(int, input().split() ))
 values = list(map(int, input().split() ))
+values.sort()
 
 for i in range(m):
 	max_card, num = list(map(int, input().split() ))
 
-	values.sort()
-	for j in range(max_card):
-		if values[j] < num:
-			values[j] = num
-		else:
-			break
+	num_small = bisect.bisect(values, num)
+	num_to_change = min(max_card, num_small)
+
+	# sort
+	values = values[num_to_change:num_small] + [num]*num_to_change + values[num_small:]
 
 print (sum(values))
