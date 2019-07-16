6794037bcd9a9b39c30d3a553413890ffbda464e
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2019-07-16 18:11:29 +0900

    ABC121 D問題

--- abc121d.py
+++ abc121d.py
@@ -1,7 +1,20 @@
 a, b = list(map(int, input().split()))
 
 # cumlative_xor(a) を 0　から a までのxorの結果とする。
-# 求める答えは、cumlative_xor(b) xor cumlative_xor(a-1)
+# 求める答えは、cumlative_xor(b) xor cumlative_xor(a-1) （累積XOR）
 
 def cumlative_xor(a):
+	ans = 0
+	if a % 4 == 1 or a % 4 == 2 :
+		ans = 1
 
+	for i in range(1, 41):
+		# 10**12 < 2**40 なので
+		temp = 2 ** i
+		if a & 1 == 0 and a & temp > 0:
+		# if a % 2 == 0 and a % (temp * 2) >= temp: と同値。
+			ans = ans | temp
+
+	return ans
+
+print(cumlative_xor(b) ^ cumlative_xor(a-1))
