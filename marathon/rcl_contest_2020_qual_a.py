40b9235f6924b643cea9640c160bfa4ba9b60e95
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2020-02-15 22:26:06 +0900

    no message

--- marathon/rcl_contest_2020_qual_a.py
+++ marathon/rcl_contest_2020_qual_a.py
@@ -1,3 +1,22 @@
-for i in range(1000):
-    print(i % 8)
-    
+
+ans_list = []
+ans_list += [j for j in range(i+1) for i in range(8)]
+ans_list += [i&7 for i in range(1000-72)]
+ans_list += [j for j in range(i, 8) for i in range(8)]
+
+for rep in range(10000):
+    pass
+
+for i in ans_list:
+    print(i) 
+
+# for i in range(8):
+#     for j in range(i+1):
+#         print(j)
+
+# for i in range(1000 - 72):
+#     print(i % 8)
+
+# for i in range(8):
+#     for j in range(i, 8):
+#         print(j)
