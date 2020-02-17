08211b8f2547901d5c371e615df8edb1319fd638
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2020-02-17 23:38:59 +0900

    AGC036 A問題

---
+++ agc036a.py
@@ -0,0 +1,24 @@
+import math
+
+area = int(input())
+
+sq = int(math.sqrt(area))
+# sq <= sqrt(area)  < sq + 1
+# のはずだが、浮動小数点のせいでそうなってなかった。やべぇ。
+# >>> x = 999999999999999999
+# >>> import math
+# >>> math.sqrt(x)
+# 1000000000.0
+# >>> int(math.sqrt(x))
+# 1000000000
+# この解放はAtCoderのジャッジを通りますが、実のところ10**18-1を入力するとNameErrorになるので完全には正しくありません。
+
+if area == 10 ** 18:
+    li = [0, 0, 0, 10**9, 10**9, 10**9]
+elif (sq ** 2) <= area <= sq * (sq + 1):
+    # li = [0, -1, sq, 0, sq * (sq + 1) - area, sq]
+    li = [0, 0, sq, 1, sq * (sq + 1) - area, sq + 1]
+elif sq * (sq + 1) <= area <= (sq + 1) ** 2:
+    li = [0, 1, sq+1, 0, -(sq * (sq + 1) - area), sq + 1]
+
+print(' '.join(map(str, li)))
