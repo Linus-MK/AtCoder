e047aa614033f30cf5a5a0c93d09aa2f77fcce41
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2020-02-24 11:29:14 +0900

    ABC156 D問題 TLE→AC

--- ABC_6q/abc156d.py
+++ ABC_6q/abc156d.py
@@ -5,22 +5,10 @@ mod = 10**9 + 7
 import time
 start = time.time()
 
-
-# x ** a をmodで割った余りを、O(log(a))時間で求める。
-def power(x, a):
-	if a == 0:
-		return 1
-	elif a == 1:
-		return x
-	elif a % 2 == 0:
-		return power(x, a//2) **2 % mod
-	else:
-		return power(x, a//2) **2 * x % mod
-
 # xの逆元を求める。フェルマーの小定理より、 x の逆元は x ^ (mod - 2) に等しい。計算時間はO(log(mod))程度。
 # https://qiita.com/Yaruki00/items/fd1fc269ff7fe40d09a6
 def modinv(x):
-	return power(x, mod-2)
+	return pow(x, mod-2, mod)
 
 modinv_table = [-1] * (b+1)
 for i in range(1, b+1):
@@ -38,7 +26,7 @@ def binomial_coefficients2(n, k):
     return ans
 
 # (2 ** n - 1) - nCa - nCb
-ans = power(2, n) - 1
+ans = pow(2, n, mod) - 1
 
 ans -= (binomial_coefficients2(n, a) + binomial_coefficients2(n, b))
 print(ans % mod)
