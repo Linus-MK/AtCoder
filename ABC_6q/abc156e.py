5a5bca8823ac0583c22e7de47e6be5ff55771b71
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2020-02-24 11:44:01 +0900

    ABC156 E問題 WAを消してTLEのみ

--- ABC_6q/abc156e.py
+++ ABC_6q/abc156e.py
@@ -2,23 +2,21 @@ n, k = list(map(int, input().split()))
 
 mod = 10**9 + 7
 
-mod = 10**9 + 7
-
 # x ** a をmodで割った余りを、O(log(a))時間で求める。
 def power(x, a):
-	if a == 0:
-		return 1
-	elif a == 1:
-		return x
-	elif a % 2 == 0:
-		return power(x, a//2) **2 % mod
-	else:
-		return power(x, a//2) **2 * x % mod
+    if a == 0:
+        return 1
+    elif a == 1:
+        return x
+    elif a % 2 == 0:
+        return power(x, a//2) **2 % mod
+    else:
+        return power(x, a//2) **2 * x % mod
 
 # xの逆元を求める。フェルマーの小定理より、 x の逆元は x ^ (mod - 2) に等しい。計算時間はO(log(mod))程度。
 # https://qiita.com/Yaruki00/items/fd1fc269ff7fe40d09a6
 def modinv(x):
-	return power(x, mod-2)
+    return power(x, mod-2)
 
 # 二項係数の左側の数字の最大値を max_len　とする。nとかだと他の変数と被りそうなので。
 # factori_table = [1, 1, 2, 6, 24, 120, ...] 要は factori_table[n] = n!
@@ -29,12 +27,14 @@ max_len = 2 * n - 1 #適宜変更する
 factori_table = [1] * (max_len + 1)
 factori_inv_table = [1] * (max_len + 1)
 for i in range(1, max_len + 1):
-	factori_table[i] = factori_table[i-1] * (i) % mod
-	factori_inv_table[i] = modinv(factori_table[i])
+    factori_table[i] = factori_table[i-1] * (i) % mod
+    factori_inv_table[i] = modinv(factori_table[i])
 
 def binomial_coefficients(n, k):
-	# n! / (k! * (n-k)! )
-	return (factori_table[n] * factori_inv_table[k] * factori_inv_table[n-k]) % mod
+    # n! / (k! * (n-k)! )
+    if k <= 0 or k >= n:
+        return 1
+    return (factori_table[n] * factori_inv_table[k] * factori_inv_table[n-k]) % mod
 
 if k >= n-1:
     # nHn = 2n-1 C n
@@ -44,11 +44,12 @@ else:
     # 人数0の部屋がちょうどj個のものは
     # nCj(人数0の部屋の選び方) * jH(n-j) (余剰のj人を残りの部屋に入れる)
     ans = 0
-    for j in range(k):
+    for j in range(k+1):
         if j == 0:
             ans += 1
         else:
-            ans += binomial_coefficients(n, j) * binomial_coefficients(n-1, j-1) 
+            ans += binomial_coefficients(n, j) * binomial_coefficients(n-1, j) 
             ans %= mod
     print(ans)
 
+# 22583772
