b4eb14e04eada981453b4066a0367c5ab1b0f27b
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2020-02-24 11:53:21 +0900

    ライブラリ：二項係数　インデントなど変更

--- Library/binomial_coefficients.py
+++ Library/binomial_coefficients.py
@@ -7,33 +7,33 @@ mod = 10**9 + 7
 
 # x ** a をmodで割った余りを、O(log(a))時間で求める。
 def power(x, a):
-	if a == 0:
-		return 1
-	elif a == 1:
-		return x
-	elif a % 2 == 0:
-		return power(x, a//2) **2 % mod
-	else:
-		return power(x, a//2) **2 * x % mod
+    if a == 0:
+        return 1
+    elif a == 1:
+        return x
+    elif a % 2 == 0:
+        return power(x, a//2) ** 2 % mod
+    else:
+        return power(x, a//2) ** 2 * x % mod
 
 # xの逆元を求める。フェルマーの小定理より、 x の逆元は x ^ (mod - 2) に等しい。計算時間はO(log(mod))程度。
 # https://qiita.com/Yaruki00/items/fd1fc269ff7fe40d09a6
 def modinv(x):
-	return power(x, mod-2)
+    return power(x, mod-2)
 
 # 二項係数の左側の数字の最大値を max_len　とする。nとかだと他の変数と被りそうなので。
 # factori_table = [1, 1, 2, 6, 24, 120, ...] 要は factori_table[n] = n!
 # 計算時間はO(max_len * log(mod))
 
-max_len = 30 #適宜変更する
+max_len = 30  # 適宜変更する
 
 factori_table = [1] * (max_len + 1)
 factori_inv_table = [1] * (max_len + 1)
 for i in range(1, max_len + 1):
-	factori_table[i] = factori_table[i-1] * (i) % mod
-	factori_inv_table[i] = modinv(factori_table[i])
+    factori_table[i] = factori_table[i-1] * (i) % mod
+    factori_inv_table[i] = modinv(factori_table[i])
+
 
 def binomial_coefficients(n, k):
-	# n! / (k! * (n-k)! )
-	return (factori_table[n] * factori_inv_table[k] * factori_inv_table[n-k]) % mod
-
+    # n! / (k! * (n-k)! )
+    return (factori_table[n] * factori_inv_table[k] * factori_inv_table[n-k]) % mod
