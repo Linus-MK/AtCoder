1e4d4ebd2be9f9c268d2a3a5f01a54ba081ed721
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2020-02-24 12:32:20 +0900

    ライブラリ：二項係数　アルゴリズム改良

--- Library/binomial_coefficients.py
+++ Library/binomial_coefficients.py
@@ -3,37 +3,52 @@
 # https://qiita.com/Yaruki00/items/fd1fc269ff7fe40d09a6
 # https://www.hamayanhamayan.com/entry/2018/06/06/210256
 
+max_len = 30  # 適宜変更する
 mod = 10**9 + 7
 
-# x ** a をmodで割った余りを、O(log(a))時間で求める。
-def power(x, a):
-    if a == 0:
-        return 1
-    elif a == 1:
-        return x
-    elif a % 2 == 0:
-        return power(x, a//2) ** 2 % mod
-    else:
-        return power(x, a//2) ** 2 * x % mod
 
-# xの逆元を求める。フェルマーの小定理より、 x の逆元は x ^ (mod - 2) に等しい。計算時間はO(log(mod))程度。
-# https://qiita.com/Yaruki00/items/fd1fc269ff7fe40d09a6
 def modinv(x):
-    return power(x, mod-2)
+    '''
+    xの逆元を求める。フェルマーの小定理より、 x の逆元は x ^ (mod - 2) に等しい。計算時間はO(log(mod))程度。
+    Python標準のpowは割った余りを出すことも可能。
+    '''
+    return pow(x, mod-2, mod)
+
 
 # 二項係数の左側の数字の最大値を max_len　とする。nとかだと他の変数と被りそうなので。
 # factori_table = [1, 1, 2, 6, 24, 120, ...] 要は factori_table[n] = n!
 # 計算時間はO(max_len * log(mod))
-
-max_len = 30  # 適宜変更する
-
+modinv_table = [-1] * (max_len + 1)
+modinv_table[0] = None  # 万が一使っていたときにできるだけ早期に原因特定できるようにしたいので、Noneにしておく。
 factori_table = [1] * (max_len + 1)
 factori_inv_table = [1] * (max_len + 1)
 for i in range(1, max_len + 1):
     factori_table[i] = factori_table[i-1] * (i) % mod
-    factori_inv_table[i] = modinv(factori_table[i])
+
+modinv_table[1] = 1
+for i in range(2, max_len + 1):
+    modinv_table[i] = (-modinv_table[mod % i] * (mod // i)) % mod
+    factori_inv_table[i] = factori_inv_table[i-1] * modinv_table[i] % mod
 
 
 def binomial_coefficients(n, k):
-    # n! / (k! * (n-k)! )
+    '''
+    n! / (k! * (n-k)! )
+    0 <= k <= nを満たさないときは変な値を返してしまうので、先にNoneを返すことにする。
+    場合によっては0のほうが適切かもしれない。
+    '''
+    if not 0 <= k <= n:
+        return None
     return (factori_table[n] * factori_inv_table[k] * factori_inv_table[n-k]) % mod
+
+
+def binomial_coefficients2(n, k):
+    '''
+    (n * (n-1) * ... * (n-k+1)) / (1 * 2 * ... * k)
+    '''
+    ans = 1
+    for i in range(k):
+        ans *= n-i
+        ans *= modinv_table[i + 1]
+        ans %= mod
+    return ans
