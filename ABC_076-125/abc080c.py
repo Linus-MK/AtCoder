df609d192723d4329b770ae8bd131c3c12af36c0
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2020-03-11 22:53:34 +0900

    ABC080 C問題

---
+++ ABC_076-125/abc080c.py
@@ -0,0 +1,23 @@
+# bit全探索
+
+n = int(input())
+eigyou = [list(map(int, input().split())) for _ in range(n)]
+profits = [list(map(int, input().split())) for _ in range(n)]
+# 全休業を除く = 0を除外して、1〜1023まで回す
+# 10 = 0000001010 ならば、木曜午前と金曜午前に営業することを示す
+
+ans = -(10 ** 7) * 100 - 10
+for i in range(1, 2**10):
+    eigyou_here = [-1] * 10
+    temp = 0
+    for digit in range(10):
+        eigyou_here[10 - 1 - digit] = int(i & (2 ** digit) > 0)
+    for j_shop in range(n):
+        coincide_time = 0
+        for time in range(10):
+            coincide_time += eigyou[j_shop][time] * eigyou_here[time]
+        temp += profits[j_shop][coincide_time]
+
+    ans = max(ans, temp)
+
+print(ans)
