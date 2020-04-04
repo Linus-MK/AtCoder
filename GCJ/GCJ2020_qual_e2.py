214f532d3d587c0b94d596a65ecec66fb90445f0
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2020-04-04 22:47:00 +0900

    GCJ2020 予選E問題

--- GCJ/GCJ2020_qual_e2.py
+++ GCJ/GCJ2020_qual_e2.py
@@ -9,14 +9,15 @@ N = int(input())
 # perm_list = permutations(range(1, N+ 1)) 
 # だとイテレータができるので、1回最後まで行くとそこで探索終了になる。リストにする必要あり。
 perm_list = list(permutations(range(1, N+ 1)))
-
+count = 0
 
 def check(matrix_old, n_row, perm):
     # n_row は 0-index
     global perm_list
+    global count
 
     matrix = matrix_old + [perm]
-    print(n_row, matrix)
+    # print(n_row, matrix)
 
     # ラテン方陣として不適格か判定する
     for row in range(n_row):
@@ -26,13 +27,18 @@ def check(matrix_old, n_row, perm):
                 return False
 
     if n_row == N-1:
-        print('found a new laten matrix')
-        print(matrix)
+        # print('found a new laten matrix')
+        # print(matrix)
+        count += 1
+
+
     else:
         for perm_new in perm_list:
-            print('appending...', n_row, list(perm_new) )
+            # print('appending...', n_row, list(perm_new) )
             check(matrix, n_row + 1, list(perm_new))
 
 
 for perm in perm_list:
     check([], 0, list(perm))
+
+print(count)
