b9cd6e62c8c2b3ef6cf0120ca2c4628cbfd80181
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2020-09-25 23:52:55 +0900

    ABC161 F問題 修正

--- ABC_6q/abc161f.py
+++ ABC_6q/abc161f.py
@@ -1,17 +1,13 @@
 # 方針は解説と全く同じなので割愛
-# なんで2のときだけ特別になった?
-# get_divisors(1) が[1]となるべきところで空配列[]になっているせいで、
-# 最終的な答えが1小さくなってしまっていた。
 
 n = int(input())
-
-if n == 2:
-    print(1)
-    exit()
 
 # 約数列挙
 # ABC136Eより
 def get_divisors(num):
+    if num == 1:
+        return [1]
+    
     divisors = []
     for d in range(1, num):
         if d * d > num:
