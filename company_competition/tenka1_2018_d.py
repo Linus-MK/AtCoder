d176f3acad88979b1c9bb25db46933a6edcac40f
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2020-09-30 23:38:39 +0900

    Tenka1 Programmer Beginner Contest D AC

--- company_competition/tenka1_2018_d.py
+++ company_competition/tenka1_2018_d.py
@@ -4,7 +4,9 @@
 # あとはN個のチームの総当たり戦を作るイメージで構成すれば良い。
 # kから考えるのがポイントのように思う。
 
-# 2つTLE (N= 87990, 99681)。ケース21(55611)で1252 ms
+# TLEは消えた。実行時間124ms
+# combi自体は悪くない。combiを何周もイテレートすると計算量がO(k^3)=O(N√N)になって厳しい。
+# combiを1周にして計算量をO(k^2)=O(N)に抑える。
 
 import itertools
 
@@ -17,14 +19,18 @@ while True:
         print(k)
 
         combi = list(itertools.combinations(range(k), 2))
+
+        dict_ = {i: [] for i in range(k)}
+        for idx, tup in enumerate(combi):
+            a = tup[0]
+            dict_[a].append(idx+1)
+            b = tup[1]
+            dict_[b].append(idx+1)
+
         for i in range(k):
-            t = [idx+1 for idx, tup in enumerate(combi) if i in tup]
-            
-            print(f'{k-1} ' + ' '.join(list(map(str, t))))
+            print(f'{k-1} ' + ' '.join(list(map(str, dict_[i]))))
 
         exit()
     elif k*(k-1)//2 > n:
         print('No')
         exit()
-
-
