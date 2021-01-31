6d9e081e1fd3f5806f12058ee64e6237cd552bf1
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2021-02-01 00:06:29 +0900

    AGC047 A問題

---
+++ AGC/agc047a.py
@@ -0,0 +1,80 @@
+n = int(input())
+
+def factorize(n):
+    '''
+    素因数分解をする。
+    タプルの配列にしようと思ったけど、逐次割り算する過程で次数を増やせないじゃん。
+    二重配列?
+    dictにしよう。
+    →2と5の素因数の数がわかれば良いので、div > 5ならその時点でリターン
+    '''
+    if n == 1:
+        # raise('n >= 2')
+        return {}
+    
+    factor = {}
+    div = 2
+    while True:
+        if div * div > n:
+            factor[n] = factor.get(n, 0) + 1
+            return factor
+
+        if n % div == 0:
+            n //= div
+            factor[div] = factor.get(div, 0) + 1
+        else:
+            div += 1
+
+            if div > 5:
+                return factor
+
+
+count_dict = {}
+
+for i in range(n):
+    string = input()
+    dot_pos = string.find('.')
+    if dot_pos == -1:
+        # 小数点がない = 整数
+        fraction_len = 0
+    else:
+        fraction_len = len(string) - dot_pos - 1
+    
+    x = int(string.replace('.', ''))
+    factor = factorize(x)
+    factor_2 = factor.get(2, 0) - fraction_len
+    factor_2 = min(9, factor_2)
+    factor_5 = factor.get(5, 0) - fraction_len
+    factor_5 = min(9, factor_5)
+
+    count_dict[(factor_2, factor_5)] = count_dict.get((factor_2, factor_5), 0) + 1
+
+# print(count_dict)
+
+table = [[0 for _ in range(20)] for _ in range(20)]
+for i in reversed(range(20)):
+    for j in reversed(range(20)):
+        if j+1 < 20:
+            table[i][j] = table[i][j+1]
+        for i0 in range(19, i-1, -1):
+            table[i][j] += count_dict.get((i0-10, j-10), 0)
+
+ans = 0
+for i in range(-9, 9+1):
+    for j in range(-9, 9+1):
+
+        count = count_dict.get((i, j), 0)
+        if count > 0:
+            temp = table[-i+10][-j+10]
+            if i >= 0 and j >= 0:
+                temp -= 1
+            ans += count * temp
+            # print(-i, -j, temp)
+
+# temp = 0
+# for factor_2 in range(0, 9+1):
+#     for factor_5 in range(0, 9+1):
+#         temp += count_dict.get((factor_2, factor_5), 0)
+
+# ans += temp * (temp-1) // 2
+print(ans//2)
