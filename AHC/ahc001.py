130ae5e34d8b5bc4086efe9f036572de8f48641d
Linus-MK <13600386+Linus-MK@users.noreply.github.com>
2021-03-14 12:08:07 +0900

    なんか変な結果になった

---
+++ AHC/ahc001.py
@@ -0,0 +1,110 @@
+import math
+import random
+
+def overlap(coords_0, coords_1):
+    return ((not (coords_0[2] <= coords_1[0] or coords_1[2] <= coords_0[0])) and
+    (not (coords_0[3] <= coords_1[1] or coords_1[3] <= coords_0[1])))
+
+
+def calc_position(width, height, center_x, center_y):
+    xmin = x-width
+    ymin = y-height
+    xmax = x+1+width
+    ymax = y+1+height
+
+    # 平行移動
+    if xmin < 0:
+        xmax += 0-xmin
+        xmin = 0
+    if ymin < 0:
+        ymax += 0-ymin
+        ymin = 0
+    if xmax > 10000:
+        xmin -= xmax-10000
+        xmax = 10000
+    if ymax > 10000:
+        ymin -= ymax-10000
+        ymax = 10000
+    
+    return [xmin, ymin, xmax, ymax]
+
+# サンプル 422343342 点
+
+n = int(input())
+
+xs = []
+ys = []
+rs = []
+width_list = []
+
+ans_list = [[0 for _ in range(4)] for _ in range(n)]
+for i in range(n):
+    x, y, r = list(map(int, input().split()))
+    xs.append(x)
+    ys.append(y)
+    rs.append(r)
+
+    # ans_list[i][0] = x
+    # ans_list[i][1] = y
+    # ans_list[i][2] = x+1
+    # ans_list[i][3] = y+1
+
+    width = int(math.sqrt(r*3//5))//2
+    width_list.append(width)
+    ans_list[i] = calc_position(width, width, x, y)
+    # ans_list[i][0] = max(x-width, 0)
+    # ans_list[i][1] = max(y-width, 0)
+    # ans_list[i][2] = min(x+1+width, 10000)
+    # ans_list[i][3] = min(y+1+width, 10000)
+    
+for i in range(n):
+    for j in range(i+1, n):
+        count = 0
+        count_limit = 30
+        while count <= count_limit:
+            if overlap(ans_list[i], ans_list[j]):
+
+                if random.random() < 0.5:
+                    width = width_list[i]
+                    width = width * 9 // 10
+                    width_list[i] = width
+                    ans_list[i] = calc_position(width, width, xs[i], ys[i]).copy()
+                    # ans_list[i][0] = max(xs[i]-width, 0)
+                    # ans_list[i][1] = max(ys[i]-width, 0)
+                    # ans_list[i][2] = min(xs[i]+1+width, 10000)
+                    # ans_list[i][3] = min(ys[i]+1+width, 10000)
+                else:
+                    width = width_list[j]
+                    width = width * 9 // 10
+                    width_list[j] = width
+                    ans_list[j] = calc_position(width, width, xs[j], ys[j]).copy()
+                    # ans_list[j][0] = max(xs[j]-width, 0)
+                    # ans_list[j][1] = max(ys[j]-width, 0)
+                    # ans_list[j][2] = min(xs[j]+1+width, 10000)
+                    # ans_list[j][3] = min(ys[j]+1+width, 10000)
+
+
+                count += 1
+            
+            else:
+                break
+        
+        if count == count_limit and overlap(ans_list[i], ans_list[j]):
+            # 少しずつ縮めていってもまだ重なってるようなら、強制的に縦横1にします
+            ans_list[i][0] = xs[i]
+            ans_list[i][1] = ys[i]
+            ans_list[i][2] = xs[i]+1
+            ans_list[i][3] = ys[i]+1
+ 
+            ans_list[j][0] = xs[j]
+            ans_list[j][1] = ys[j]
+            ans_list[j][2] = xs[j]+1
+            ans_list[j][3] = ys[j]+1
+
+for i in range(n):
+    print(f'{ans_list[i][0]} {ans_list[i][1]} {ans_list[i][2]} {ans_list[i][3]}')
+
+
+print(xs)
+print(ys)
+print([id(ele) for ele in ans_list])
